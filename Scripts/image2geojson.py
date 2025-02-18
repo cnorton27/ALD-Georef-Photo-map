a027df1e3cef0d768b32c07b6e8524eb464fa496
cnorton27 <ciaranorton@hotmail.com>
2025-02-18 09:35:15 -0800

    Created main script to call processing scripts. Also updated virtual links to images in geojson to match new Github directory structure.

--- Scripts/image2geojson.py
+++ Scripts/image2geojson.py
@@ -104,27 +104,5 @@ def get_date_data(image_path):
     return [date, utc_time]
 
 
-output_geojson = r'C:\GitHub\ALD-Georef-Photo-map\Flood_photos_18Feb2025.json'
-ald_photos = r'C:\GitHub\ALD-Georef-Photo-map\ALD_photos_2024_processed'
-flood_photos = r'C:\GitHub\ALD-Georef-Photo-map\FloodPhotos'
-
-# Example usage:
-# features2collection('/path/to/images', '/path/to/output.geojson', 'string of dataset category")
-
-#features2collection(flood_photos, output_geojson, "Flood")
-#features2collection(ald_photos, output_geojson, "ALD/Slump")
-#features2collection(veg_photos, output_geojson, "Vegetation")
-#features2collection(Drone_photos, output_geojson, "Drone")
-
-#Use image_add_virtual_path.py script to add virtual file paths as a property to feature collection
-
-from image_add_virtual_path import adding_path
-
-data = r'C:\GitHub\ALD-Georef-Photo-map\Flood_photos_18Feb2025.json'
-image_base_path = "https://raw.githubusercontent.com/cnorton27/ALD-Georef-Photo-map/main/ALD_photos_2024_processed/"
-out_path = r'C:\GitHub\ALD-Georef-Photo-map\ALD_photos_with_path.geojson'
-
-adding_path(image_base_path, data, out_path)
-
 
 
